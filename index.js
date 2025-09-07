e0916400c2b796311518c6dae3c9e367a993e115
KHALED70000 <abdullah151337@gmail.com>
2025-09-07 16:57:57 +0600

    All Tree button added with its functionality

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-function AllCategories() {
+function MainFunction() {
     let AllcataURL = "https://openapi.programming-hero.com/api/categories"
+
+
+     window.selectAll = (id, e) => {
+
+        
+
+
+        let plantURL = `https://openapi.programming-hero.com/api/plants`;
+        fetch(plantURL)
+            .then(res => res.json())
+            .then(data => {
+                let PLANTS = data.plants;
+                const CART_CONTAINER = document.getElementById("cart_container");
+                CART_CONTAINER.innerHTML = "";
+                PLANTS.forEach(plant => {
+
+                    CART_CONTAINER.innerHTML += `
+                    <div id="cart" class="bg-white shadow-sm h-[400px] p-4 rounded-lg flex flex-col space-y-4">
+                        <div class="w-full h-[200px] overflow-hidden flex items-center justify-center rounded-lg">
+                            <img src="${plant.image}" alt="demo" class="w-full h-full object-cover"/>
+                        </div>
+                        <h4 class="text-[14px] font-inter font-semibold text-[#1F2937]">${plant.name}</h4>
+                        <p class="w-full overflow-hidden text-ellipsis line-clamp-2 text-[12px] font-inter text-[#1F2937]">asdfasdfasdf asdfasdffs
+                            asdfasdfasdf asdfasdfasdf asdfasdfasdf asdfasdf asdfasd sdasdf asdfsadf asdfasdf asdf asdf
+                        </p>
+
+                        <div class="flex justify-between items-center">
+                            <span class="bg-[#CFF0DC] rounded-full py-[2px] px-[10px] text-[14px] font-Geist font-medium text-[#15803D]">${plant.name}</span>
+                            <span class="text-[14px] font-Geist font-semibold text-[#1F2937]">৳${plant.price}</span>
+                        </div>
+
+                        <button class="bg-[#15803D] text-white w-full px-4 py-2 rounded-full cursor-pointer">Add to Cart</button>
+                    </div>
+                `;
+
+                });
+            });
+    }
 
     window.selectCategory = (id, e) => {
 
@@ -44,12 +82,14 @@ function AllCategories() {
 
                 });
             });
-    }
+    };
     fetch(AllcataURL)
         .then(res => res.json())
         .then(data => {
             let categoriesNames = data.categories;
             let cataContainer = document.getElementById("cataContainer");
+            cataContainer.innerHTML = `
+            <li onclick="selectAll(event)" class="cursor-pointer text-[#1F2937] text-[16px] py-[10px] px-[8px] rounded hover:bg-green-200 hover:text-black"> All Trees</li>`;
 
             categoriesNames.forEach(cataName => {
                 let CataSerial = cataName.id;
@@ -61,5 +101,7 @@ function AllCategories() {
 
         });
 
-}
-AllCategories();
+};
+
+MainFunction();
+selectAll();
